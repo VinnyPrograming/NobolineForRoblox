00f5152641041026dab1ab24dd5ba6dccb0639a2
Vinny <103543816+VinnyPrograming@users.noreply.github.com>
2022-09-28 12:14:41 -0400

    Update testfire.lua

--- source/testfire.lua
+++ source/testfire.lua
@@ -345,3 +345,41 @@ local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPa
     end
 )
 end)
+
+ction:NewButton("kill all", "tp bypass", function()
+		--// Setting \\--
+local range = 15
+ 
+--// Variable \\--
+local player = game:GetService("Players").LocalPlayer
+ 
+--// Script \\--
+game:GetService("RunService").RenderStepped:Connect(function()
+    local p = game.Players:GetPlayers()
+    for i = 2, #p do local v = p[i].Character
+        if v and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 and v:FindFirstChild("HumanoidRootPart") and player:DistanceFromCharacter(v.HumanoidRootPart.Position) <= range then
+            local tool = player.Character and player.Character:FindFirstChildOfClass("Tool")
+            if tool and tool:FindFirstChild("Handle") then
+                tool:Activate()
+                for i,v in next, v:GetChildren() do
+                    if v:IsA("BasePart") then
+                        firetouchinterest(tool.Handle,v,0)
+                        firetouchinterest(tool.Handle,v,1)
+                    end
+                end
+            end
+        end
+    end
+end)
+
+
+game.RunService.RenderStepped:Connect(function()
+    for i, v in pairs(game:GetService("Players"):GetChildren()) do
+        if v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Team ~= game.Players.LocalPlayer.Team then
+            print(v.Name .. " is the current target!")
+            repeat wait(0.2)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame - Vector3.new(0,0,5)
+            until v.Character.Humanoid.Health == 0 or not v.Character:FindFirstChild("Humanoid")
+        end
+    end
+end)
