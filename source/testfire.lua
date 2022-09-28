0d215df81360299e82a2b9f2f8c809f806b3637f
Vinny <103543816+VinnyPrograming@users.noreply.github.com>
2022-09-28 12:20:29 -0400

    Update testfire.lua

--- source/testfire.lua
+++ source/testfire.lua
@@ -346,7 +346,7 @@ local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPa
 )
 end)
 
-Section:NewButton("KillAura", "Attacks players that come to you.", function()
+Section:NewButton("kill all", "Attacks players that come to you.", function()
     --// Setting \\--
 local range = 15
  
