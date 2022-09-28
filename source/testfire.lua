e61fe4c61604056ce392b8afcdf08e6b36b4c679
Vinny <103543816+VinnyPrograming@users.noreply.github.com>
2022-09-28 11:58:50 -0400

    Update testfire.lua

--- source/testfire.lua
+++ source/testfire.lua
@@ -314,5 +314,34 @@ end)
 end)
 
 Section:NewButton("TP Bypass (CTRL + Click)", "tp bypass", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/VinnyPrograming/NobolineForRoblox/main/source/548097549.lua'))()
+    local Plr = game:GetService("Players").LocalPlayer
+local Mouse = Plr:GetMouse()
+ 
+Mouse.Button1Down:connect(
+    function()
+        if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
+            return
+        end
+        if not Mouse.Target then
+            return
+        end
+        local maxDistance = 145;
+        local vectorDistanceBetweenHRPnTarget = (Mouse.Hit.p - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+
+        if vectorDistanceBetweenHRPnTarget >= maxDistance then
+            print("Safe TP started!")
+             local TweenService = game:GetService("TweenService")
+local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(10, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+
+{CFrame = CFrame.new(Mouse.Hit.p)}):Play()
+return
+        end
+
+        local TweenService = game:GetService("TweenService")
+local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+
+{CFrame = CFrame.new(Mouse.Hit.p)}):Play()
+
+    end
+)
 end)
